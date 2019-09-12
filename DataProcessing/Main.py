7520492ff8fc2167c3d9f6045c0a3c5758ab6540
usi <nicky.zimmerman@usi.ch>
2019-09-12 11:34:06 -0700

    supporting Dario's dataset

--- DataProcessing/Main.py
+++ DataProcessing/Main.py
@@ -36,12 +36,43 @@ def CreateDatasetFromRosbag(rosbagName, pickleName, isBebop=True, start_frame=No
 		dc.CreateHimaxDataset(config.himax_delay, pickleName, start_frame, end_frame)
 
 
+def CreateDatasetFromDarioRosbag(rosbagName, pickleName, start_frame=None, end_frame=None):
+	"""Converts Dario's rosbag to format suitable for training/testing. 
+	if start_frame, end_frame are unknown, FrameSelector will help you choose how to trim the video
+
+    Parameters
+    ----------
+    rosbagName : str
+        The file location of the rosbag
+    pickleName : str
+        name of the new .pickle file
+    start_frame : int, optional
+        if known, the timestamp in ns of the frame you wish to start from 
+    end_frame : int, optional
+        if known, the timestamp in ns of the frame you wish to finish at
+    """
+
+	dc = DatasetCreator(rosbagName)
+	if (start_frame is None) or (end_frame is None):
+		start_frame, end_frame = dc.FrameSelector(True)
+
+	dc.CreateBebopDarioDataset(0, pickleName, start_frame, end_frame)
+
+		
+
+
+
 
 def main():
-	subject_name = "davide1"
-	rosbagName = config.folder_path + "/data/Hand/" + subject_name + ".bag"
-	pickleName = config.folder_path + "/data/Hand/" + subject_name + "Hand.pickle"
-	CreateDatasetFromRosbag(rosbagName, pickleName, isBebop=True, start_frame=None, end_frame=None)
+	# subject_name = "davide1"
+	# rosbagName = config.folder_path + "/data/Hand/" + subject_name + ".bag"
+	# pickleName = config.folder_path + "/data/Hand/" + subject_name + "Hand.pickle"
+	# CreateDatasetFromRosbag(rosbagName, pickleName, isBebop=True, start_frame=None, end_frame=None)
+
+	subject_name = "3"
+	rosbagName = config.folder_path + "/data/" + subject_name + ".bag"
+	pickleName = config.folder_path + "/data/" + subject_name + ".pickle"
+	CreateDatasetFromDarioRosbag(rosbagName, pickleName, start_frame=None, end_frame=None)
 
 	# If you wish to join several .pickle files into one big .pickle, use
 	# DatasetCreator.JoinPickleFiles(fileList, newPickleName, folderPath)
