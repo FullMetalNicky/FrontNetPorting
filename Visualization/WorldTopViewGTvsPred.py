0b9965ac864157338dee46ddedb32af6678b3510
FullMetalNicky <>
2020-02-28 13:40:53 +0100

    fixed axes and added grid

--- Visualization/WorldTopViewGTvsPred.py
+++ Visualization/WorldTopViewGTvsPred.py
@@ -17,7 +17,23 @@ from Dataset import Dataset
 from ModelManager import ModelManager
 
 
+def MoveToWorldFrame(head, himax):
+    phi = head[3] + himax[3] + np.pi
+
+    rotation = np.array([[np.cos(himax[3]), -np.sin(himax[3])], [np.sin(himax[3]), np.cos(himax[3])] ])
+    xy = np.array([[head[0], head[1]]]).transpose()
+    xy = rotation @ xy
+    xy = xy + np.array([[himax[0], himax[1]]]).transpose()
+    x, = xy[0]
+    y, = xy[1]
+
+    return x, y, phi
+
+# def PlotGrid():
+#     plt.plot()
+
 def VizWorldTopView(frames, labels, camPoses, outputs, isGray=False):
+
     fig = plt.figure(888, figsize=(15, 8))
 
     h = 9
@@ -30,32 +46,46 @@ def VizWorldTopView(frames, labels, camPoses, outputs, isGray=False):
     ax0.axis('off')
 
     ax1 = plt.subplot2grid((h, w), (2, 0), colspan=8, rowspan=7)
-    ax1.set_title('World Frame Pose (x,y)')
+    ax1.set_title('World Frame Pose')
     ax1.yaxis.set_ticks([-3, -1.5, 0, 1.5, 3])  # set y-ticks
-    ax1.xaxis.set_ticks([-3.0, -1.5, 0, 1.5, 3.0])  # set y-ticks
+    #ax1.xaxis.set_ticks([3.0, 1.5, 0, -1.5, -3.0])  # set y-ticks
+    plt.xlim(3.0, -3.0)
+    plt.ylim(-3.0, 3.0)
+
     ax1.xaxis.tick_top()  # and move the X-Axis
     ax1.yaxis.tick_left()  # remove right y-Ticks
     ax1.spines['right'].set_visible(False)
     ax1.spines['bottom'].set_visible(False)
+    ax1.set_xlabel('Y')
+    ax1.set_ylabel('X')
+    #plt.gca().invert_xaxis()
 
 
     plot1gt, = plt.plot([], [], color='green', label='GT', linestyle='None', marker='o', markersize=10)
-    plot1pr, = plt.plot([], [], color='blue', label='Prediction', linestyle='None', marker='o', markersize=10)
-    plot1cam, = plt.plot([], [], color='red', label='Camera', linestyle='None', marker='o', markersize=10)
+    plot1pr, = plt.plot([], [], color='blue', label='Prediction', linestyle='None', marker='^', markersize=10)
+    plot1cam, = plt.plot([], [], color='k', label='Camera', linestyle='None', marker='s', markersize=10)
     arr1gt = ax1.arrow([], [], np.cos([]), np.sin([]), head_width=0.1, head_length=0.1, color='green', animated=True)
     arr1pr = ax1.arrow([], [], np.cos([]), np.sin([]), head_width=0.1, head_length=0.1, color='blue', animated=True)
-    arr1cam = ax1.arrow([], [], np.cos([]), np.sin([]), head_width=0.1, head_length=0.1, color='red', animated=True)
+    arr1cam = ax1.arrow([], [], np.cos([]), np.sin([]), head_width=0.1, head_length=0.1, color='k', animated=True)
+    plt.plot([2.4, -2.4], [2.4, 2.4], color='gray', linestyle='solid')
+    plt.plot([2.4, -2.4], [-2.4, -2.4], color='gray', linestyle='solid')
+
+    plt.plot([2.4, 2.4], [2.4, -2.4], color='gray', linestyle='solid')
+    plt.plot([-2.4, -2.4], [2.4, -2.4], color='gray', linestyle='solid')
+
+    plt.plot([2.4, -2.4], [1.2, 1.2], color='gray', linestyle='solid')
+    plt.plot([2.4, -2.4], [0, 0], color='gray', linestyle='solid')
+    plt.plot([2.4, -2.4], [-1.2, -1.2], color='gray', linestyle='solid')
+
+    plt.plot([1.2, 1.2], [2.4, -2.4], color='gray', linestyle='solid')
+    plt.plot([0, 0], [2.4, -2.4], color='gray', linestyle='solid')
+    plt.plot([-1.2, -1.2], [2.4, -2.4], color='gray', linestyle='solid')
+
+
+
     plt.legend(loc='lower right', bbox_to_anchor=(0.8, 0.2, 0.25, 0.25))
 
-    # ax2 = plt.subplot2grid((h, w), (2, 8), rowspan=7)
-    # ax2.set_title('Relative z', pad=20)
-    # ax2.yaxis.tick_right()
-    # ax2.set_xlim([-0.5, 0.5])
-    # ax2.set_xticklabels([])
-    # ax2.yaxis.set_ticks([-1, 0, 1])  # set y-ticks
-    # ax2.xaxis.set_ticks_position('none')
-    # scatter2gt = plt.scatter([], [], color='green', label='GT', s=100)
-    # scatter2pr = plt.scatter([], [], color='blue', label='Prediction', s=100)
+
 
     ax3 = plt.subplot2grid((h, w), (2, 9), rowspan=7, colspan=7)
     ax3.axis('off')
@@ -79,46 +109,44 @@ def VizWorldTopView(frames, labels, camPoses, outputs, isGray=False):
     writer = Writer(fps=10, metadata=dict(artist='FullMetalNicky'))
 
     def animate(id):
+
         label = labels[id]
-        x_gt, y_gt, z_gt, phi_gt = label[0], label[1], label[2], label[3]
-        x_pred, y_pred, z_pred, phi_pred = outputs[id * 4 + 0], outputs[id * 4 + 1], outputs[id * 4 + 2], outputs[
-            id * 4 + 3]
         camPose = camPoses[id]
+        pred = [outputs[id * 4 + 0], outputs[id * 4 + 1], outputs[id * 4 + 2], outputs[id * 4 + 3]]
+        x_gt, y_gt, phi_gt = MoveToWorldFrame(label, camPose)
+        x_pred, y_pred, phi_pred = MoveToWorldFrame(pred, camPose)
+
         x_cam, y_cam, z_cam, phi_cam = camPose[0], camPose[1], camPose[2], camPose[3]
 
-        # str1 = "x_gt={:05.3f}, y_gt={:05.3f}, z_gt={:05.3f}, phi_gt={:05.3f} {}".format(x_gt, y_gt, z_gt, phi_gt, "\n")
-        # str1 = str1 + "x_pr={:05.3f}, y_pr={:05.3f}, z_pr={:05.3f}, phi_pr={:05.3f}".format(x_pred, y_pred, z_pred,
-        #                                                                                     phi_pred)
+        str1 = "x_cam={:05.3f}, y_cam={:05.3f}, phi_cam={:05.3f} {}".format(x_cam, y_cam, phi_cam, "\n")
+        str1 = str1 + "x_gt={:05.3f}, y_gt={:05.3f}, phi_gt={:05.3f} {}".format(x_gt, y_gt, phi_gt, "\n")
+        str1 = str1 + "x_pr={:05.3f}, y_pr={:05.3f},  phi_pr={:05.3f}".format(x_pred, y_pred, phi_pred)
 
-        str1 = "x_gt={:05.3f}, y_gt={:05.3f}, z_gt={:05.3f}, phi_gt={:05.3f} {}".format(x_cam, y_cam, z_cam, phi_cam, "\n")
 
-        phi_gt = - phi_gt - np.pi / 2
-        phi_pred = -phi_pred - np.pi / 2
+        # phi_gt = - phi_gt - np.pi / 2
+        # phi_pred = -phi_pred - np.pi / 2
 
         annotation.set_text(str1)
 
-        # plot1gt.set_data(np.array([y_gt, x_gt]))
-        # plot1pr.set_data(np.array([y_pred, x_pred]))
+        plot1gt.set_data(np.array([y_gt, x_gt]))
+        plot1pr.set_data(np.array([y_pred, x_pred]))
         plot1cam.set_data(np.array([y_cam, x_cam]))
 
         if (len(ax1.patches) > 1):
             ax1.patches.pop()
             ax1.patches.pop()
+            ax1.patches.pop()
 
-        # patch1 = patches.FancyArrow(y_gt, x_gt, 0.5 * np.cos(phi_gt), 0.5 * np.sin(phi_gt), head_width=0.05,
-        #                             head_length=0.05, color='green')
-        # patch2 = patches.FancyArrow(y_pred, x_pred, 0.5 * np.cos(phi_pred), 0.5 * np.sin(phi_pred), head_width=0.05,
-        #                             head_length=0.05, color='blue')
+        patch1 = patches.FancyArrow(y_gt, x_gt, 0.5 * np.sin(phi_gt), 0.5 * np.cos(phi_gt), head_width=0.05,
+                                    head_length=0.05, color='green')
+        patch2 = patches.FancyArrow(y_pred, x_pred, 0.5 * np.sin(phi_pred), 0.5 * np.cos(phi_pred), head_width=0.05,
+                                    head_length=0.05, color='blue')
+        patch3 = patches.FancyArrow(y_cam, x_cam, 0.5 * np.sin(phi_cam), 0.5 * np.cos(phi_cam), head_width=0.05,
+                                    head_length=0.05, color='k')
 
-        patch3 = patches.FancyArrow(y_cam, x_cam, 0.5 * np.sin( -phi_cam), 0.5 * np.cos(-phi_cam), head_width=0.05,
-                                    head_length=0.05, color='red')
-
-        # ax1.add_patch(patch1)
-        # ax1.add_patch(patch2)
+        ax1.add_patch(patch1)
+        ax1.add_patch(patch2)
         ax1.add_patch(patch3)
-
-        # scatter2gt.set_offsets(np.array([-0.05, label[2]]))
-        # scatter2pr.set_offsets(np.array([0.05, outputs[id * 4 + 2]]))
 
         frame = frames[id].astype(np.uint8)
         if isGray == False:
@@ -131,7 +159,7 @@ def VizWorldTopView(frames, labels, camPoses, outputs, isGray=False):
         # return plot1gt, plot1pr, patch1, patch2, scatter2gt, scatter2pr, imgplot, ax1, ax3, annotation, annotation2
         #return plot1gt, plot1pr, patch1, patch2, scatter2gt, scatter2pr, imgplot, annotation, annotation2
 
-        return plot1gt, plot1pr, imgplot, annotation, annotation2
+        return plot1gt, plot1pr, plot1cam, patch1, patch2, patch3, imgplot, annotation, annotation2
 
     ani = animation.FuncAnimation(fig, animate, frames=len(frames), interval=1, blit=True)
     ani.save('WorldTopView.mp4', writer=writer)
