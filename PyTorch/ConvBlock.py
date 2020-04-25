15c2eea599f9fd5970326a1878e1e5396ca10438
FullMetalNicky <>
2020-04-25 11:49:45 +0200

    changed NN name to HannaNet

--- PyTorch/ConvBlock.py
+++ PyTorch/ConvBlock.py
@@ -4,7 +4,7 @@ import logging
 np.set_printoptions(threshold=np.inf)
 np.set_printoptions(suppress=True)
 
-class PreActBlock(nn.Module):
+class ConvBlock(nn.Module):
     '''Pre-activation version of the BasicBlock.'''
     expansion = 1
 
@@ -12,8 +12,8 @@ class PreActBlock(nn.Module):
     # I just wanted to keep the previous API. I will clean it up once you approve the net
 
     def __init__(self, in_planes, planes, stride=1):
-        super(PreActBlock, self).__init__()
-        self.bn1 = nn.BatchNorm2d(in_planes)
+        super(ConvBlock, self).__init__()
+        self.bn1 = nn.BatchNorm2d(planes)
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.relu1 = nn.ReLU()
         self.bn2 = nn.BatchNorm2d(planes)
@@ -25,21 +25,22 @@ class PreActBlock(nn.Module):
 
     def forward(self, x):
 
-        out = self.bn1(x)
+        out = self.conv1(x)
+        out = self.bn1(out)
         out = self.relu1(out)
-        #printRes(out, "ReLU")
-        shortcut = self.shortcut(x) if hasattr(self, 'shortcut') else x
-        out = self.conv1(out)
+
+        out = self.conv2(out)
         out = self.bn2(out)
         out = self.relu2(out)
-        #printRes(out, "3x3ConvReLU")
-        out = self.conv2(out)
-        #printRes(out, "3x3Conv")
-        #printRes(shortcut, "1x1Conv")
-        out += shortcut
+
         return out
 
 def printRes(layer, name):
-    n, c, h, w = layer.shape
-    tmp = layer.reshape(-1)
-    logging.info("{}={}".format(name, list(tmp.numpy())))
+
+    logger = logging.getLogger('')
+    enable = logger.isEnabledFor(logging.INFO)
+    yes = layer.requires_grad
+    if (enable == True):
+        n, c, h, w = layer.shape
+        tmp = layer.reshape(-1)
+        logging.info("{}={}".format(name, list(tmp.detach().numpy())))
--- PyTorch/HannaNet.py
+++ PyTorch/HannaNet.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
-from PreActBlock import PreActBlock
-from PreActBlock import printRes
+from ConvBlock import ConvBlock
+from ConvBlock import printRes
 import numpy as np
 import logging
 
@@ -11,14 +11,14 @@ def conv1x1(in_planes, out_planes, stride=1):
     """1x1 convolution"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
-class Dronet(nn.Module):
+class HannaNet(nn.Module):
     def __init__(self, block, layers, isGray=False):
-        super(Dronet, self).__init__()
+        super(HannaNet, self).__init__()
 
         if isGray ==True:
-            self.name = "DronetGray"
+            self.name = "HannaNetGray"
         else:
-            self.name = "DronetRGB"
+            self.name = "HannaNetRGB"
         self.inplanes = 32
         self.dilation = 1
         self._norm_layer = nn.BatchNorm2d
@@ -32,9 +32,9 @@ class Dronet(nn.Module):
 
         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
-        self.layer1 = PreActBlock(32, 32, stride=2)
-        self.layer2 = PreActBlock(32, 64, stride=2)
-        self.layer3 = PreActBlock(64, 128, stride=2)
+        self.layer1 = ConvBlock(32, 32, stride=2)
+        self.layer2 = ConvBlock(32, 64, stride=2)
+        self.layer3 = ConvBlock(64, 128, stride=2)
 
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout()
@@ -48,31 +48,28 @@ class Dronet(nn.Module):
 
     def forward(self, x):
 
-        out = self.conv(x)
-        out = self.maxpool(out)
-        #printRes(out, "5x5ConvMax_1")
-        out = self.layer1(out)
-        #printRes(out, "Add_1")
-        out = self.layer2(out)
-        #printRes(out, "Add_2")
-        out = self.layer3(out)
-        out = out.view(out.size(0), -1)
-        out = self.relu(out)
-        #logging.info("AddReLU_3")
-        #logging.info(list(out.numpy()))
-        out = self.dropout(out)
-        x = self.fc_x(out)
-        #logging.info("Dense1={}".format(x.numpy()))
-        y = self.fc_y(out)
-        #logging.info("Dense2={}".format(y.numpy()))
-        z = self.fc_z(out)
-        #logging.info("Dense3={}".format(z.numpy()))
-        phi = self.fc_phi(out)
-        #logging.info("Dense4={}".format(phi.numpy()))
+        conv5x5 = self.conv(x)
+        max_pool = self.maxpool(conv5x5)
+
+        l1 = self.layer1(max_pool)
+        l2 = self.layer2(l1)
+        l3 = self.layer3(l2)
+        flat = l3.view(l3.size(0), -1)
+        relu = self.relu(flat)
+
+        drop = self.dropout(relu)
+        x = self.fc_x(drop)
+        y = self.fc_y(drop)
+        z = self.fc_z(drop)
+        phi = self.fc_phi(drop)
+
+        # PrintFC(x, "Dense1")
+        # PrintFC(y, "Dense2")
+        # PrintFC(z, "Dense3")
+        # PrintFC(phi, "Dense4")
 
         return [x, y, z, phi]
 
-    
 
 def PrintRelu(layer, name):
     logger = logging.getLogger('')
