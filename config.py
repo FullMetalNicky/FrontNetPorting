1d5008ae826dc26365417517d7e88dfaa23b9b73
FullMetalNicky <nickyfullmetal@gmail.com>
2021-03-24 20:47:03 +0100

    removing unneeded files/folders 2

--- config.py
+++
@@ -1,8 +0,0 @@
-folder_path = "/home/usi/Documents/Drone/FrontNetPorting"
-himax_width = 324
-himax_height = 244
-input_width = 108
-input_height = 60
-himax_delay = -1817123289
-optitrack_delay = 4777750000
-dronemarker_offset = 0
